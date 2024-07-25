cba57c014b8d7dfe4c748a52947db0a843dd1131
Raj kapure <rajkapure5@gmail.com>
2024-07-25 14:47:13 +0530

    new one file

--- src/newproject1/NewClass.java
+++ src/newproject1/NewClass.java
@@ -1,5 +1,6 @@
 package newproject1;
 
+
 import java.time.Duration;
 import java.util.List;
 
@@ -7,6 +8,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class NewClass {
 
@@ -19,16 +23,21 @@ public class NewClass {
 		driver.findElement(By.name("username")).sendKeys("Admin");
 		driver.findElement(By.name("password")).sendKeys("admin123");
 		driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']")).click();
-		Thread.sleep(5000);
-		driver.findElement(By.xpath("//div[@id='app']/div[1]/div[1]/aside/nav/div[2]/ul/li[3]/a")).click();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-	driver.findElement(By.xpath("//div[@id='app']/div[1]/div[1]/header/div[2]/nav/ul/li[3]")).click();
-	List<WebElement>p=driver.findElements(By.xpath("//div[@id='app']/div[1]/div[1]/header/div[2]/nav/ul/li[3]/ul//li"));
-		System.out.println(p.size());
-		for(WebElement r:p) {
-			String s=r.getText();
-			System.out.println(s);
-		}
+driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.xpath("//span[text()='Recruitment']")).click();
+		Select s=new Select(driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/div/div/div[2]/i")));
+		s.selectByVisibleText("Account Assistant");
+	//driver.findElement(By.xpath("//div[@id='app']/div[1]/div[1]/header/div[2]/nav/ul/li[3]")).click();
+	//List<WebElement> p=driver.findElements(By.xpath("//nav[@aria-label='Topbar Menu']//a"));
+		//System.out.println(p.size());
+		//for(WebElement r:p) {
+		//	String s=r.getText();
+			//r.click();
+			//System.out.println(s);
+			//Thread.sleep(4000);
+			//p=driver.findElements(By.xpath("//nav[@aria-label='Topbar Menu']//a"));
+		//}
+	
 			}
 		}
 		
